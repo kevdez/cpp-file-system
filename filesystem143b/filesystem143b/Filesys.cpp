2756bb07e0baef5c2c3ba43eef15bdc1350e16af
kevdez <kernan373@gmail.com>
2014-06-02 22:01:38 -0700

    added FileSys:: scoping to the 10 FS functions. Forgot that.

--- filesystem143b/filesystem143b/Filesys.cpp
+++ filesystem143b/filesystem143b/Filesys.cpp
@@ -7,52 +7,52 @@ FileSys::FileSys()
 {
 }
 
-std::string create(std::string name)
+std::string FileSys::create(std::string name)
 {
 	return "";
 }
 
-std::string destroy(std::string name)
+std::string FileSys::destroy(std::string name)
 {
 	return "";
 }
 
-std::string open(std::string name)
+std::string FileSys::open(std::string name)
 {
 	return "";
 }
 
-std::string close(int index)
+std::string FileSys::close(int index)
 {
 	return "";
 }
 
-std::string read(int index, int count)
+std::string FileSys::read(int index, int count)
 {
 	return "";
 }
 
-std::string write(int index, char c, int count)
+std::string FileSys::write(int index, char c, int count)
 {
 	return "";
 }
 
-std::string seek(int index, int pos)
+std::string FileSys::seek(int index, int pos)
 {
 	return "";
 }
 
-std::string directory()
+std::string FileSys::directory()
 {
 	return "";
 }
 
-std::string initialize(std::string name = NULL)
+std::string FileSys::initialize(std::string name)
 {
 	return "";
 }
 
-std::string save(std::string outputFile)
+std::string FileSys::save(std::string outputFile)
 {
 	return "";
 }
